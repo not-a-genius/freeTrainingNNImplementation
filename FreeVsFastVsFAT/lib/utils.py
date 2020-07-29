dc851dfb98b3a12af8428cdc8b5b10af3c3bfb3a
not-a-genius <romagiuppo@gmail.com>
2020-07-29 19:51:40 +0200

    merged

--- FreeVsFastVsFAT/lib/utils.py
+++ FreeVsFastVsFAT/lib/utils.py
@@ -124,6 +124,7 @@ def clamp(X, lower_limit, upper_limit):
     return torch.max(torch.min(X, upper_limit), lower_limit)
 
 
+<<<<<<< HEAD
 def get_loaders(dir_, batch_size, num_workers):
     configs=[]
     with open("configs.yml") as f:
@@ -136,6 +137,11 @@ def get_loaders(dir_, batch_size, num_workers):
         # transforms.Normalize(cifar10_mean, cifar10_std),
 
         transforms.RandomResizedCrop(configs.DATA.crop_size),
+=======
+def get_loaders(dir_, batch_size, num_workers, crop_size):
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(crop_size, padding=4),
+>>>>>>> 6f975008b19ec4896df8c4013d0a7f5bcc677cd0
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
     ])
